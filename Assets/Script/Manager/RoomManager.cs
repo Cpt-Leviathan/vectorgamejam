7c9e3a392749996ffcbe2359f2b984ba89650235
VictorPref <VictorPref@users.noreply.github.com>
2020-01-31 22:31:23 -0500

    Basic Manager
    PlayerManager,RoomManager,SpaceshipManager,ToolsManager,UIManager

---
+++ Assets/Script/Manager/RoomManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager 
+{
+
+    private static RoomManager instance = null;
+
+    private RoomManager()
+    {
+    }
+
+    public static RoomManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new RoomManager();
+            }
+            return instance;
+        }
+    }
+
+    public void init()
+    {
+
+
+    }
+
+    public void Update()
+    {
+
+
+    }
+
+    public void FixedUpdate()
+    {
+
+
+    }
+
+}
---
+++ Assets/Script/Manager/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager 
+{
+
+    private static UIManager instance = null;
+
+    private UIManager()
+    {
+    }
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new UIManager();
+            }
+            return instance;
+        }
+    }
+
+    public void init()
+    {
+
+
+    }
+
+    public void Update()
+    {
+
+
+    }
+
+    public void FixedUpdate()
+    {
+
+
+    }
+}
