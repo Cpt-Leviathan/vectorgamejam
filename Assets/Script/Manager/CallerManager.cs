3b42927b0f66e35087271859f56471d613ee9a03
Cpt-Leviathan <50187291+Cpt-Leviathan@users.noreply.github.com>
2020-02-02 06:41:59 -0500

    Update CallerManager.cs

--- Assets/Script/Manager/CallerManager.cs
+++ Assets/Script/Manager/CallerManager.cs
@@ -25,7 +25,7 @@ public class CallerManager
 
     public void init()
     {
-        GameObject go = GameObject.Find("New Sprite");
+        GameObject go = GameObject.FindGameObjectWithTag("caller");
         caller = go.GetComponent<Caller>();
         if (caller != null)
         {
